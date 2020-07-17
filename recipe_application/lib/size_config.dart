47d84ac48e9836b386261821e0ef92279b4bb4af
Truong Hoang Minh <truong.hoang@verztec.com>
2020-07-18 00:08:38 +0700

    Init Models NavItem and Recipe

---
+++ recipe_application/lib/size_config.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class SizeConfig {
+  static MediaQueryData _mediaQueryData;
+  static double screenWidth;
+  static double screenHeight;
+  static double defaultSize;
+  static Orientation orientation;
+
+  init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenHeight = _mediaQueryData.size.height;
+    screenWidth = _mediaQueryData.size.width;
+    orientation = _mediaQueryData.orientation;
+    defaultSize = orientation == Orientation.landscape ? screenHeight * 0.024 : screenWidth * 0.024;
+  }
+}
